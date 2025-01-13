1944bfbd05e0f3dcb1c45bbd263ed68bfbf57b27
kaloronahuang <kaloronahuang@outlook.com>
2025-01-13 09:48:37 -0600

    Using abspath for LTP build

--- build-ltp.sh
+++ build-ltp.sh
@@ -3,7 +3,7 @@ git clone --recurse-submodules $LTP_REPO ltp
 cd ltp
 git checkout $LTP_BRANCH
 make autotools
-LINUX_VERSION=$(make -C ../linux -s kernelversion)
-./configure --with-linux-version=$LINUX_VERSION --with-linux-dir=../linux
+LINUX_VERSION=$(make -C /opt/ltp-build/linux -s kernelversion)
+./configure --with-linux-version=$LINUX_VERSION --with-linux-dir=/opt/ltp-build/linux
 make -j$(nproc)
 make DESTDIR=/opt/ltp-build/ltp-deliverable install
